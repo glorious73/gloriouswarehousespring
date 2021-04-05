c7b2ef9162763812188bfcec09c2f422422ccc10
glorious73 <amjadmh73@gmail.com>
2021-04-05 20:49:35 +0300

    Home only accepts a get request

--- src/main/java/com/glorious73/gloriouswarehouse/controller/HomeController.java
+++ src/main/java/com/glorious73/gloriouswarehouse/controller/HomeController.java
@@ -1,11 +1,12 @@
 package com.glorious73.gloriouswarehouse.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String Main() {
         return "Hello world from Spring Boot!";
     }
